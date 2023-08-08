9b9b0872257116aa6379f7ef69b3bb3530f5b908
songhsb <hssong404@gmail.com>
2023-08-08 20:37:01 +0900

    Design: 마이페이지 마크 업

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { styled } from 'styled-components';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
+import Layout from '../components/common/layout/Layout';
 
 const MyPage = () => {
   return (
-    <div>MyPage</div>
-  )
-}
+    <Layout>
+      <ProfileBox>
+        <Avatar size={64} icon={<UserOutlined />} />
+        닉네임 이메일
+      </ProfileBox>
+      <ListSection>
+        <ListBox>내가 좋아요 한 코스</ListBox>
+        <ListBox>내가 쓴 댓글 목록</ListBox>
+      </ListSection>
+    </Layout>
+  );
+};
 
-export default MyPage
+const ProfileBox = styled.div`
+  height: 150px;
+  margin-bottom: 15px;
+  background-color: gray;
+`;
+
+const ListSection = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  flex-flow: row wrap;
+`;
+
+const ListBox = styled.div`
+  height: 500px;
+  flex-basis: 500px;
+  flex-grow: 1;
+  background-color: gray;
+`;
+
+export default MyPage;
