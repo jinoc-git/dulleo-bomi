7da5bdc0ef4e9b1a778d6ab592134df4c4774752
jinoc <choiae369@gmail.com>
2023-08-08 19:17:13 +0900

    Feat: 메인 베너

--- src/components/banner/style.ts
+++ src/components/banner/style.ts
@@ -1,11 +1,25 @@
 import { styled } from 'styled-components';
 
 export const BannerContainer = styled.section`
+  position: relative;
   width: 100%;
-  border: 1px solid;
+  height: 540px;
 `;
 
-export const BannerTitle = styled.h3`
+export const BanneImage = styled.img`
+  width: 100%;
+  min-height: 420px;
+  max-height: 600px;
+  object-fit: cover;
+  filter: contrast(1.1);
+`;
 
-`
-
+export const BannerParagraph = styled.p`
+  position: absolute;
+  top: 80px;
+  right: 50px;
+  color: #fff;
+  font-size: 24px;
+  line-height: 1.8;
+  text-align: right;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { aa } from '../api/course';
 import { useQuery } from '@tanstack/react-query';
 import Header from '../components/header/Header';
 import Banner from '../components/banner/Banner';
 
 const Home = () => {
-  const { data } = useQuery(['data'], aa);
-  if (data) {
-    console.log(data);
-  }
+
   return (
     <>
       <Header />
