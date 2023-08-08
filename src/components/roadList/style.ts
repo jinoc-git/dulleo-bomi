5483a864700a2a2dbcf467d2724062fe323d8d96
jinoc <choiae369@gmail.com>
2023-08-08 20:50:09 +0900

    Feat: 메인 페이지 길 설명 이미지, navigate 추가

---
+++ src/components/roadList/style.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const RoadListContainer = styled.section`
+  & > div {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+export const RoadItem = styled.img`
+  cursor: pointer;
+  width: 340px;
+`
