af8d2c6b30263be98665f2c82a3cf552b45e1462
HyunseoY <maricat0715@gmail.com>
2023-08-09 17:28:35 +0900

    Feat: 회원가입 성공과 동시에 유저정보 auth와 firestore에 각각 저장
    폼 css 수정. 모든 인풋 길이 동일하게.
    회원가입 버튼 색상 수정. 프로젝트 메인 블루컬러로 변경

--- src/components/SignUp/SignUpForm.tsx
+++ src/components/SignUp/SignUpForm.tsx
@@ -1,44 +1,60 @@
-import { Button, Form, Input } from 'antd';
+import { Form, FormInstance, Input, message } from 'antd';
 import { FirebaseError } from 'firebase/app';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { useState } from 'react';
-import { auth, storage } from '../../firebase/firebaseConfig';
+import { ReactElement, useState } from 'react';
+import { auth, db, storage } from '../../firebase/firebaseConfig';
 import * as St from './style';
 
 type SignUpFormData = {
-  profileImage: FileList;
+  profileImage: File[];
   email: string;
   password: string;
   confirmPassword: string;
   nickname: string;
 };
 
-const SignUpForm = () => {
+const SignUpForm = (): ReactElement => {
+  const [form] = Form.useForm();
   const [error, setError] = useState<string | null>(null);
   const [emailError, setEmailError] = useState<string | null>(null);
 
-  const onSubmit = async (data: SignUpFormData) => {
+  const onSubmit = async (values: unknown) => {
+    const data = values as SignUpFormData;
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.password);
       const { user } = userCredential;
 
-      if (user && data.profileImage.length > 0) {
-        const filePath = `profiles/${user.uid}/profile_picture`;
-        const imageRef = ref(storage, filePath);
-        const file = data.profileImage[0];
-
-        await uploadBytes(imageRef, file);
-        const downloadURL = await getDownloadURL(imageRef);
-
-        await updateProfile(user, {
+      if (user) {
+        const userData = {
+          uid: user.uid,
+          email: data.email,
           displayName: data.nickname,
-          photoURL: downloadURL,
-        });
+          photoURL: '',
+        };
+
+        if (data.profileImage.length > 0) {
+          const filePath = `profiles/${user.uid}/profile_picture`;
+          const imageRef = ref(storage, filePath);
+          const file = data.profileImage[0];
+
+          await uploadBytes(imageRef, file);
+          const downloadURL = await getDownloadURL(imageRef);
+
+          await updateProfile(user, {
+            displayName: data.nickname,
+            photoURL: downloadURL,
+          });
+
+          userData.photoURL = downloadURL; // 이미지 다운로드 URL을 데이터에 추가
+        }
+
+        // 파이어스토어 'users' 컬렉션에 데이터 추가
+        await addDoc(collection(db, 'users'), userData);
       }
-      // 알럿으로 바꾸기
-      alert('회원가입이 성공적으로 처리되었습니다');
-      console.log('회원가입 성공:', userCredential);
+
+      message.success('회원가입이 성공적으로 처리되었습니다');
       setError(null);
     } catch (err) {
       if (err instanceof FirebaseError) {
@@ -63,18 +79,18 @@ const SignUpForm = () => {
     },
   });
 
-  // const confirmPasswordValidator = (formInstance: FormInstance) => ({
-  //   validator(_: unknown, value: string) {
-  //     if (!value || formInstance.getFieldValue('password') === value) {
-  //       return Promise.resolve();
-  //     }
-  //     return Promise.reject(new Error('비밀번호가 일치하지 않습니다.'));
-  //   },
-  // });
+  const confirmPasswordValidator = (formInstance: FormInstance) => ({
+    validator(_: unknown, value: string) {
+      if (!value || formInstance.getFieldValue('password') === value) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('비밀번호가 일치하지 않습니다.'));
+    },
+  });
 
   return (
     <St.SignUpFormContainer>
-      <Form onFinish={onSubmit}>
+      <St.SignUpForm form={form} onFinish={onSubmit} name="validateOnly" layout="vertical" autoComplete="off">
         <Form.Item label="프로필 이미지" name="profileImage" rules={[{ required: true, message: '이미지를 업로드해주세요.' }]}>
           <Input type="file" id="profileImage" />
         </Form.Item>
@@ -96,21 +112,7 @@ const SignUpForm = () => {
           <Input type="password" id="password" />
         </Form.Item>
 
-        <Form.Item
-          label="비밀번호 확인"
-          name="confirmPassword"
-          rules={[
-            { required: true, message: '비밀번호를 한 번 더 입력해주세요.' },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error('비밀번호가 일치하지 않습니다.'));
-              },
-            }),
-          ]}
-        >
+        <Form.Item label="비밀번호 확인" name="confirmPassword" rules={[{ required: true, message: '비밀번호를 한 번 더 입력해주세요.' }, confirmPasswordValidator(form)]}>
           <Input type="password" id="confirmPassword" />
         </Form.Item>
 
@@ -118,12 +120,12 @@ const SignUpForm = () => {
           <Input type="text" id="nickname" />
         </Form.Item>
         {error && <p>{error}</p>}
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
+        <St.ButtonContainer>
+          <St.Button type="default" htmlType="submit">
             회원가입
-          </Button>
-        </Form.Item>
-      </Form>
+          </St.Button>
+        </St.ButtonContainer>
+      </St.SignUpForm>
     </St.SignUpFormContainer>
   );
 };
--- src/components/SignUp/style.ts
+++ src/components/SignUp/style.ts
@@ -1,4 +1,4 @@
-import { Form as AntdForm } from 'antd';
+import { Button as AntdButton, Form as AntdForm } from 'antd';
 import { styled } from 'styled-components';
 
 export const SignUpFormContainer = styled.div`
@@ -10,4 +10,28 @@ export const SignUpFormContainer = styled.div`
 
 export const SignUpForm = styled(AntdForm)`
   width: 50%;
+`;
+
+export const ButtonContainer = styled(AntdForm.Item)`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const Button = styled(AntdButton)`
+  background-color: #9acdde;
+  border-color: transparent;
+  color: #ffffff;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+
+  &:hover {
+    background-color: #b8e3e8;
+    border-color: transparent;
+    color: #ffffff;
+  }
+
+  &:active {
+    background-color: #67bed0;
+    border-color: transparent;
+    color: #ffffff;
+  }
 `;
