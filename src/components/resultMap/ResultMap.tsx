234f55595dd924c7efd8dc8ae0bebcd0840a0288
congjiwon <sheepg1510@gmail.com>
2023-08-10 21:54:52 +0900

    feat: 지도 서버 완료

--- src/components/resultMap/ResultMap.tsx
+++ src/components/resultMap/ResultMap.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import * as St from './style';
 import Layout from '../common/layout/Layout';
+import { Map } from 'react-kakao-maps-sdk';
+import axios from 'axios';
+
 const ResultMap = () => {
+  const GPX_URL = `https://www.durunubi.kr/editImgUp.do?filePath=/data/koreamobility/file/2021/09/46e0055b28ac46ea9420106c8939fa61.gpx`;
+  console.log(GPX_URL);
+
+  const fetchGPX = async () => {
+    const res = await axios.get(`https://florentine-rustic-open.glitch.me/gpx?data=${GPX_URL}`);
+
+    // const parser: GpxParser = new GpxParser();
+    // const gpxJson: GpxJson = await parser.parse(GPX_URL);
+    // const response = await axios.get(GPX_URL);
+    // console.log(response);
+    // return response;
+
+    // console.log(gpxJson);
+
+    fetch(GPX_URL)
+      .then((response) => response.text())
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  fetchGPX();
+
   return (
     <Layout>
-      <St.ResultMapContainer>지도영역(해당코스 전부 마커표시)</St.ResultMapContainer>
+      <St.ResultMapContainer>
+        <Map // 지도를 표시할 Container
+          center={{
+            // 지도의 중심좌표
+            lat: 33.450701,
+            lng: 126.570667,
+          }}
+          style={{
+            // 지도의 크기
+            width: '100%',
+            height: '400px',
+          }}
+          level={13} // 지도의 확대 레벨
+        />
+      </St.ResultMapContainer>
     </Layout>
   );
 };
