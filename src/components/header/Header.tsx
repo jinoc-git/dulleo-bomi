94d8ec982b8d4a76c6fc7076907cb31ed4581ae6
HyunseoY <maricat0715@gmail.com>
2023-08-10 13:02:52 +0900

    Feat: zustand로 로그인 유저 상태 관리

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,24 @@
-import Layout from '../common/layout/Layout';
-import { Link, useNavigate } from 'react-router-dom';
-import * as St from './style';
-import { Button } from 'antd';
+import { Avatar, Button } from 'antd';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { logoColor } from '../../assets';
+import { useUserStore } from '../../zustand/UserStore';
+import Layout from '../common/layout/Layout';
+import * as St from './style';
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = useUserStore((state) => state.user);
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+  const setupAuthObserver = useUserStore((state) => state.setupAuthObserver);
+  const logout = useUserStore((state) => state.logout);
+
+  useEffect(() => {
+    const unsubscribe = setupAuthObserver();
+    return () => {
+      unsubscribe();
+    };
+  }, [setupAuthObserver]);
 
   return (
     <St.ContainerHeader>
@@ -16,8 +29,23 @@ const Header = () => {
             <St.LogoTitH1>둘러보미</St.LogoTitH1>
           </St.Logo>
           <St.UserBtnBox>
-            <Button>로그인</Button>
-            <Button>회원가입</Button>
+            {isLoggedIn && user ? (
+              <>
+                <span>{user.displayName}님 반갑습니다</span>
+                <Avatar
+                  src={user.photoURL}
+                  alt="프로필 사진"
+                  onClick={() => navigate('/mypage')}
+                  style={{ cursor: 'pointer' }}
+                />
+                <Button onClick={() => logout()}>로그아웃</Button>
+              </>
+            ) : (
+              <>
+                <Button onClick={() => navigate('/signin')}>로그인</Button>
+                <Button onClick={() => navigate('/signup')}>회원가입</Button>
+              </>
+            )}
           </St.UserBtnBox>
         </St.WrapBox>
       </Layout>
