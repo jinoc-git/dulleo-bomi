edced83789a32b852a02f3b6cb271518bbc02469
HyunseoY <maricat0715@gmail.com>
2023-08-11 15:16:12 +0900

    Feat: 공용 다이얼로그 작업 중2

--- src/components/common/customAlert/CustomAlert.tsx
+++ src/components/common/customAlert/CustomAlert.tsx
@@ -1,31 +1,12 @@
 import { CheckCircleFilled } from '@ant-design/icons';
 import { Button, Modal } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useDialogStore } from '../../../zustand/DialogStore';
 
 const CustomAlert: React.FC = () => {
   const alertVisible = useDialogStore((state) => state.alertVisible);
   const alertContent = useDialogStore((state) => state.alertContent);
   const hideAlert = useDialogStore((state) => state.hideAlert);
-
-  const [counter, setCounter] = useState(5);
-
-  useEffect(() => {
-    if (alertVisible) {
-      const timer = setInterval(() => {
-        setCounter((current) => current - 1);
-      }, 1000);
-
-      setTimeout(() => {
-        hideAlert();
-        setCounter(5);
-      }, 5000);
-
-      return () => {
-        clearInterval(timer);
-      };
-    }
-  }, [alertVisible, hideAlert]);
 
   const handleOk = () => {
     hideAlert();
@@ -44,7 +25,7 @@ const CustomAlert: React.FC = () => {
       closable={false}
       footer={[
         <Button key="ok" type="primary" onClick={handleOk}>
-          확인 ({counter}초 후 자동 닫힘)
+          확인
         </Button>,
       ]}
     >
