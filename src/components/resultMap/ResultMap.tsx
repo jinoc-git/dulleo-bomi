95025a1b94a3394045cebc93257b89b3fdd4e159
congjiwon <sheepg1510@gmail.com>
2023-08-11 14:11:14 +0900

    chore: 푸시 커밋

--- src/components/resultMap/ResultMap.tsx
+++ src/components/resultMap/ResultMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as St from './style';
 import Layout from '../common/layout/Layout';
 import { Map } from 'react-kakao-maps-sdk';
@@ -8,24 +8,19 @@ const ResultMap = () => {
   const GPX_URL = `https://www.durunubi.kr/editImgUp.do?filePath=/data/koreamobility/file/2021/09/46e0055b28ac46ea9420106c8939fa61.gpx`;
   console.log(GPX_URL);
 
-  const fetchGPX = async () => {
-    const res = await axios.get(`https://florentine-rustic-open.glitch.me/gpx?data=${GPX_URL}`);
+  useEffect(() => {
+    const fetchGPX = async () => {
+      const res = await axios.get(`https://florentine-rustic-open.glitch.me/gpx?data=${GPX_URL}`);
+      console.log(res);
 
-    // const parser: GpxParser = new GpxParser();
-    // const gpxJson: GpxJson = await parser.parse(GPX_URL);
-    // const response = await axios.get(GPX_URL);
-    // console.log(response);
-    // return response;
-
-    // console.log(gpxJson);
-
-    fetch(GPX_URL)
-      .then((response) => response.text())
-      .then((response) => {
-        console.log(response);
-      });
-  };
-  fetchGPX();
+      // fetch(GPX_URL)
+      //   .then((response) => response.text())
+      //   .then((response) => {
+      //     console.log(response);
+      //   });
+    };
+    fetchGPX();
+  }, []);
 
   return (
     <Layout>
