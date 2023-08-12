751bedcddba625c531a5fed4d05589f2b523ee38
HyunseoY <maricat0715@gmail.com>
2023-08-12 16:26:24 +0900

    Feat: 404에러 페이지 완료

--- src/components/common/errorPage/ErrorPage.tsx
+++ src/components/common/errorPage/ErrorPage.tsx
@@ -1,18 +1,36 @@
 import { Button } from 'antd';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import errorpage from '../../../assets/errorpage.png';
 import * as St from './style';
 
 const ErrorPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoToPreviousPage = () => {
+    window.history.back();
+  };
+
   return (
     <St.ErrorWrapper>
-      {/* <St.ErrorCode>404</St.ErrorCode> */}
-      <img src={errorpage} alt="404에러이미지" />
-      <St.ErrorMessage>죄송합니다. 찾으려는 페이지를 찾을 수 없습니다. </St.ErrorMessage>
-      <St.ErrorMessage>존재하지 않는 주소를 입력하셨거나, </St.ErrorMessage>
-      <St.ErrorMessage>요청하신 주소가 변경, 삭제되어 찾을 수 없습니다. </St.ErrorMessage>
-      <Button>메인으로</Button>
-      <Button>이전으로</Button>
+      <St.ErrorImg src={errorpage} alt="404에러이미지" />
+      <St.ErrorMessageBox>
+        <St.ErrorMessage>죄송합니다. 찾으려는 페이지를 찾을 수 없습니다. </St.ErrorMessage>
+        <St.ErrorMessage>존재하지 않는 주소를 입력하셨거나, </St.ErrorMessage>
+        <St.ErrorMessage>요청하신 주소가 변경, 삭제되어 찾을 수 없습니다. </St.ErrorMessage>
+      </St.ErrorMessageBox>
+      <St.ErrorBtnBox>
+        <Button
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          메인으로
+        </Button>
+        <Button type="primary" onClick={handleGoToPreviousPage}>
+          이전으로
+        </Button>
+      </St.ErrorBtnBox>
     </St.ErrorWrapper>
   );
 };
