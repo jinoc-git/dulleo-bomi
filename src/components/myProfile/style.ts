75fa29c8b79cb7f9befbf97402f8ca5b0c296d60
HyunseoY <maricat0715@gmail.com>
2023-08-13 20:56:32 +0900

    Design: 마이페이지 디자인 수정

--- src/components/myProfile/style.ts
+++ src/components/myProfile/style.ts
@@ -4,9 +4,15 @@ export const ProfileContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
-  height: 150px;
+  /* height: 150px; */
   margin-bottom: 15px;
   padding-left: 30px;
   border-radius: 10px;
-  background-color: #9acdde;
+  flex-direction: column;
+`;
+
+export const UserBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
