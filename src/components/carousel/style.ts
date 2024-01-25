4c2a0bb9f4624433e45338a0bbfd706a54c07948
jinoc <choiae369@gmail.com>
2024-01-26 03:18:43 +0900

    Feat: 코스 목록 슬라이더의 패널 클릭 시 해당 패널이 가운데로 오는 기능 추가

--- src/components/carousel/style.ts
+++ src/components/carousel/style.ts
@@ -3,12 +3,6 @@ import { styled } from 'styled-components';
 export const CarouselContainer = styled.section`
   position: relative;
 
-  & .prev-button {
-    position: absolute;
-    top: -32px;
-    left: 0;
-  }
-
   & .next-button {
     position: absolute;
     top: -32px;
